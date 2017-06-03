9efa0ba3702086dd13e58417d63657a764c7216d
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-06-03 15:43:13 +0500

    first

---
+++ server.js
@@ -0,0 +1,66 @@
+/* =====================================================
+      Importing Necessory Module & Envoirnment Setting
+   ===================================================== */
+
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var express = require('express');
+var port = process.env.PORT || '3000';
+var cors = require('cors');
+var app = express();
+// var firebase = require("firebase");
+// var admin = require("firebase-admin");
+var nodemailer = require('nodemailer');
+// import * as admin from "firebase-admin";
+
+/* =====================================================
+            Middleware Will Run on Every Request
+   ===================================================== */
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+app.post('/mailSend', mailSend)
+function mailSend(req, res) {
+    handleSayHello(req, res);
+    function handleSayHello(req, res) {
+        // create reusable transporter object using the default SMTP transport
+        var transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: 'haseebrehmanpc@gmail.com', // Your email id
+                pass: 'shan1234' // Your password
+            }
+        });
+        // var text = 'Hello your reservation confirmed. Your Slot Number is' + req.headers.slotno + ' \n\n' + req.headers.from;
+        var text =  'User submited information:'+ ' \n' + 'Name: ' + req.body.name + ' \n' + 'Email: ' + req.body.email + ' \n' + 'Contact: ' + req.body.contact + ' \n' + 'Country: ' + req.body.country + ' \n' + 'City: ' + req.body.city + ' \n' + 'Number: ' + req.body.number;
+        var mailOptions = {
+            from: 'haseebrehmanpc@gmail.com', // sender address
+            to: req.body.email, // list of receivers
+            subject: 'Your respective user email', // Subject line
+            text: text // plaintext body
+        };
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+                res.json({ response: error });
+            } else {
+                console.log('Message sent: ' + info.response);
+                res.json({ response: info.response });
+            };
+        });
+    }
+}
+
+
+
+/* =====================================================
+                      Server Listen
+   ===================================================== */
+
+app.listen(port, function () {
+    console.log("server is listening on port : ", port);
+}).timeout = 25000;
