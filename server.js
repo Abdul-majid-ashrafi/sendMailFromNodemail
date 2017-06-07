8aa99bf53838b12d83c3917dca0a944d82f6a300
Majid Ashraf <dear_majid01@yahoo.com>
2017-06-07 13:24:20 +0500

    Update server.js

--- server.js
+++ server.js
@@ -29,13 +29,13 @@ function mailSend(req, res) {
             service: 'Gmail',
             auth: {
                 user: 'majidashrafkhan2@gmail.com', // Your email id
-                pass: '' // Your password
+                pass: 'majidkhanpakistan' // Your password
             }
         });
         // var text = 'Hello your reservation confirmed. Your Slot Number is' + req.headers.slotno + ' \n\n' + req.headers.from;
         var text = 'User submited information:' + ' \n' + 'Name: ' + req.body.name + ' \n' + 'Email: ' + req.body.email + ' \n' + 'Contact: ' + req.body.contact + ' \n' + 'Country: ' + req.body.country + ' \n' + 'City: ' + req.body.city + ' \n' + 'Number: ' + req.body.number + ' \n' + 'Message: ' + req.body.message;
         var mailOptions = {
-            from: 'no-reply@Tola.com', // sender address
+            from: 'majidashrafkhan2@gmail.com', // sender address
             to: 'dear_majid01@yahoo.com', // list of receivers
             subject: 'Your respective user email', // Subject line
             text: text // plaintext body
